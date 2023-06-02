75d30ca525c49802472cf4175e057be841cde4d9
Green-Blood <bloodjey98@gmail.com>
2023-06-02 20:45:58 +0200

    Update: Some assets are added

---
+++ Assets/Scripts/OpenClosePanels.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenClosePanels : MonoBehaviour
+{
+    [SerializeField] private GameObject panelToOpen;
+    [SerializeField] private GameObject openingPanel;
+    
+    
+    
+}
