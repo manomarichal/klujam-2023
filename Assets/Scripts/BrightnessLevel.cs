78d8cb03fac818a513a51740f57427ac15d529a9
Green-Blood <bloodjey98@gmail.com>
2023-06-03 20:05:09 +0200

    Update: Brightness finished

--- Assets/Scripts/BrightnessLevel.cs
+++ Assets/Scripts/BrightnessLevel.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
@@ -12,18 +10,33 @@ public class BrightnessLevel : MonoBehaviour
     [SerializeField] private Slider volumeSlider;
 
     private ColorAdjustments _colorAdjustments;
-    private float brightnessValue;
+    private float _brightnessValue;
+
     private void Awake()
     {
+        _brightnessValue = PlayerPrefs.GetFloat("Brightness");
+
         if (volume.profile.TryGet(out _colorAdjustments))
         {
+            if (_brightnessValue != 0)
+            {
+                _colorAdjustments.contrast.value = _brightnessValue;
+            } 
+
             volumeSlider.value = _colorAdjustments.contrast.value * -1;
         }
+
         volumeSlider.onValueChanged.AddListener(OnValueChanged);
     }
 
-    private void OnValueChanged(float arg0)
+    private void OnValueChanged(float value)
     {
-        _colorAdjustments.contrast.value = arg0 * -1;
+        _colorAdjustments.contrast.value = value * -1;
+    }
+
+    private void OnDestroy()
+    {
+        volumeSlider.onValueChanged.RemoveListener(OnValueChanged);
+        PlayerPrefs.SetFloat("Brightness", volumeSlider.value * -1);
     }
 }
